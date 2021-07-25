43ec4b6e5f3414c8b1a6c288ef383a5a1b025d04
LUIS FERNANDO SANMARTIN CARRILLO <sanmhepn@hotmail.com>
2021-07-25 14:35:48 -0500

    promer commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCg1UQomG42r9z2gcyhBunk_57hwmQUyJc",
+    authDomain: "authmapgeo.firebaseapp.com",
+    projectId: "authmapgeo",
+    storageBucket: "authmapgeo.appspot.com",
+    messagingSenderId: "88207440378",
+    appId: "1:88207440378:web:fffe71a92cc81d12c70c43",
+    measurementId: "G-FXRTP8GZDY"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
